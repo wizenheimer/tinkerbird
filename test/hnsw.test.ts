47c4eec4a8bd4bc3d353ad94e32ab610c2149604
Nayan <wizenheimer@proton.me>
2023-12-09 00:15:12 +0530

    test: add cosine euc test

---
+++ test/hnsw.test.ts
@@ -0,0 +1,57 @@
+import { HNSW } from "../src";
+import { SimilarityMetric } from "../src/metric";
+import { describe, expect } from "vitest";
+
+describe("HNSW Test with Cosine Metric", (test) => {
+    test("should return correct nearest neighbors", async () => {
+        // Simple example with cosine metric
+        const hnsw = new HNSW(16, 200, 5, SimilarityMetric.cosine);
+
+        // Make some data
+        const data = [
+            { id: 1, vector: [1, 2, 3, 4, 5] },
+            { id: 2, vector: [2, 3, 4, 5, 6] },
+            { id: 3, vector: [3, 4, 5, 6, 7] },
+            { id: 4, vector: [4, 5, 6, 7, 8] },
+            { id: 5, vector: [5, 6, 7, 8, 9] }
+        ];
+
+        // Build the index
+        await hnsw.buildIndex(data);
+
+        // Search for nearest neighbors
+        const results = hnsw.query([6, 7, 8, 9, 10], 2);
+
+        expect(results).toEqual([
+            { id: 1, score: 0.9649505047327671 },
+            { id: 2, score: 0.9864400504156211 }
+        ]);
+    });
+});
+
+describe("HNSW Test with Euclidean Metric", (test) => {
+    test("should return correct nearest neighbors with euclidean distance", async () => {
+        // Simple example with euclidean distance
+        const hnsw = new HNSW(16, 200, 5, SimilarityMetric.euclidean);
+
+        // Make some data
+        const data = [
+            { id: 1, vector: [1, 2, 3, 4, 5] },
+            { id: 2, vector: [2, 3, 4, 5, 6] },
+            { id: 3, vector: [3, 4, 5, 6, 7] },
+            { id: 4, vector: [4, 5, 6, 7, 8] },
+            { id: 5, vector: [5, 6, 7, 8, 9] }
+        ];
+
+        // Build the index
+        await hnsw.buildIndex(data);
+
+        // Search for nearest neighbors
+        const results = hnsw.query([6, 7, 8, 9, 10], 2);
+
+        expect(results).toEqual([
+            { id: 1, score: 0.08209951522176571 },
+            { id: 2, score: 0.10056040392403998 }
+        ]);
+    });
+});
