ca0be255c8be18f84cdfbdfede03f260dc3dcc00
Nayan <91504165+wizenheimer@users.noreply.github.com>
2023-12-16 04:57:37 +0530

    feat: purge cache opt

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { HNSW, vectorReducer, vectorTransformer, vectorResult } from "./hnsw";
 export { VectorStore, VectorStoreOptions } from "./store";
 export { SimilarityMetric } from "./metric";
-export { CacheOptions } from "./cache";
